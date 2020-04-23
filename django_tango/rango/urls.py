944c8c774b6fdc00a366bd96cb548a4f712fdb5b
Tim Chase <timjchase@gmail.com>
2020-04-23 14:38:26 -0400

    template changes

--- django_tango/rango/urls.py
+++ django_tango/rango/urls.py
@@ -5,5 +5,7 @@ from rango import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("category/<category_name_slug>", views.show_category, name="show_category"),
+    path("add_category/", views.add_category, name="add_category"),
+    path("category/<category_name_slug>/add_page/", views.add_page, name="add_page"),
     path("about/", views.about, name="about"),
 ]
